e04decab95052f488697c579341df3e2c4026030
Rahul Dave <rahuldave@gmail.com>
2024-11-02 16:38:43 -0400

    Created the Questions

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+pixi add python
+pixi add --platform win-64 python
+pixi add --platform win-64 python
+pixi add --platform osx-64 python
+pixi add --platform linux-64 python
+pixi add numpy matplotlib scipy pandas scikit-learn jupyterlab
+pixi add --platform win-64 numpy matplotlib scipy pandas scikit-learn jupyterlab
+pixi add --platform linux-64 numpy matplotlib scipy pandas scikit-learn jupyterlab
+pixi add --platform osx-64 numpy matplotlib scipy pandas scikit-learn jupyterlab
